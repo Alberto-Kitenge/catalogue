a304c8c174068d853ffaf0ce345aa9b468787d02
Alberto Kitenge <kitengewalberto@gmail.com>
2025-10-25 07:52:53 +0100

    Blog Laravel / post with components blade

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    public function index(): View
+    {
+        return view('posts.index', [
+            'posts' => Post::latest()->paginate(5),
+        ]);
+    }
+
+    public function show(Post $post): View
+    {
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+    }
+}
